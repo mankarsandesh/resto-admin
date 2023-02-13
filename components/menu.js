0bf76f59d0d43d0b8d244ce50e39c904f2354717
Sandesh Mankar <mankarsandesh11@gmail.com>
2023-02-13 23:10:12 +0700

    Am Drunk going for sleep

--- components/menu.js
+++ components/menu.js
@@ -81,6 +81,7 @@ export default function menu() {
 											<div>
 												<Menu.Button className="flex max-w-xs items-center rounded-full bg-gray-800 text-sm text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
 													<span className="sr-only">Open user menu</span>
+													<span className="mr-4 ml-4">Sandesh Mankar</span>
 													<img
 														className="h-8 w-8 rounded-full"
 														src={user.imageUrl}
